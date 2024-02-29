c1c2b7e8b8d16c452d9cfcc2c0433412db6f70e1
AlisaLC <ali_sal1381@yahoo.com>
2024-02-29 10:12:50 +0330

    changed prompt

--- chat.py
+++ chat.py
@@ -43,10 +43,10 @@ class Chat:
                 (
                     "system",
                     """You are a helpful assistant. You are asked to answer questions about The Winter Seminar Series event as a support.
-The Winter Seminar Series (WSS) is a professional community event hosted by the Sharif University of Technology, aimed at bringing together successful Iranians globally to focus on computer science and engineering topics. Established eight years ago by the Student Scientific Chapter, WSS has become a significant four-day event where speakers present their research, share findings, and teach. The seminar includes presentations, roundtable discussions on various scientific topics, and educational workshops. These workshops are conducted online by university alumni and cover practical aspects of computer science and engineering. The event also features roundtable discussions in Persian, encouraging networking and knowledge exchange among participants.
+The Winter Seminar Series (WSS) (may also be written as وسس in Persian) is a professional community event hosted by the Sharif University of Technology, aimed at bringing together successful Iranians globally to focus on computer science and engineering topics. Established eight years ago by the Student Scientific Chapter, WSS has become a significant four-day event where speakers present their research, share findings, and teach. The seminar includes presentations, roundtable discussions on various scientific topics, and educational workshops. These workshops are conducted online by university alumni and cover practical aspects of computer science and engineering. The event also features roundtable discussions in Persian, encouraging networking and knowledge exchange among participants.
 The user has asked a question about the event. 5 of the most simialr question and answer pairs are given in below context.
 You must answer only based on the context and the information about the event already provided to you. try to give positive answers about the event.
-If asked about yourself, tell them that you are Mehrab(محراب)'s little brother.
+If asked about yourself, tell them that you are Mehrab's little brother. Mehrab is written as محراب in Persian.
 If you do not know the answer to the question, just respond with a phrase like `I do not know the answer to your question`.
 here is the context:
 
@@ -73,17 +73,36 @@ here is the context:
     def __init_transformation_chain(self):
         query_transform_prompt = ChatPromptTemplate.from_messages(
             [
+                (
+                    "system",
+                    """You are a helpful assistant. You are supposed to give a search query to find the most similar question and answer pair. here is what you should know about the event that the question and answers are about:
+The Winter Seminar Series (WSS) (may also be written as وسس in Persian) is a professional community event hosted by the Sharif University of Technology, aimed at bringing together successful Iranians globally to focus on computer science and engineering topics. Established eight years ago by the Student Scientific Chapter, WSS has become a significant four-day event where speakers present their research, share findings, and teach. The seminar includes presentations, roundtable discussions on various scientific topics, and educational workshops. These workshops are conducted online by university alumni and cover practical aspects of computer science and engineering. The event also features roundtable discussions in Persian, encouraging networking and knowledge exchange among participants.""",
+                ),
                 MessagesPlaceholder(variable_name="messages"),
                 (
                     "user",
-                    "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation. Only respond with the query, nothing else.",
+                    "Given the above conversation and your knowledge about the event, generate a search query to look up in order to get information relevant to the conversation. Only respond with the query, nothing else.",
+                ),
+            ]
+        )
+        query_base_prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """You are a helpful assistant. You are supposed to give a search query to find the most similar question and answer pair. here is what you should know about the event that the question and answers are about:
+The Winter Seminar Series (WSS) (may also be written as وسس in Persian) is a professional community event hosted by the Sharif University of Technology, aimed at bringing together successful Iranians globally to focus on computer science and engineering topics. Established eight years ago by the Student Scientific Chapter, WSS has become a significant four-day event where speakers present their research, share findings, and teach. The seminar includes presentations, roundtable discussions on various scientific topics, and educational workshops. These workshops are conducted online by university alumni and cover practical aspects of computer science and engineering. The event also features roundtable discussions in Persian, encouraging networking and knowledge exchange among participants.""",
+                ),
+                MessagesPlaceholder(variable_name="messages"),
+                (
+                    "user",
+                    "Given the above conversation and your knowledge about the event, generate a search query to look up in order to get the most relevant question and answer pair to the conversation. Only respond with the query, nothing else.",
                 ),
             ]
         )
         self.query_transforming_retriever_chain = RunnableBranch(
             (
                 lambda x: len(x.get("messages", [])) == 1,
-                (lambda x: x["messages"][-1].content) | self.retriever,
+                query_base_prompt | self.chat_api | StrOutputParser() | self.retriever,
             ),
             query_transform_prompt | self.chat_api | StrOutputParser() | self.retriever,
         ).with_config(run_name="chat_retriever_chain")
